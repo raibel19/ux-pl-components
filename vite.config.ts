01e0691684220bc2b58a3f82ccd1b8df8e165a58
Polo <raibel_19@hotmail.com>
2025-05-30 06:16:31 -0600

    feat: :sparkles: Add tsconfig paths support and update external dependencies in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,19 @@ import { extname, relative, resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({ tsconfigPath: resolve(__dirname, 'tsconfig.lib.json') })],
+  plugins: [
+    react(),
+    // tsconfigPaths({
+    //   projects: [resolve(__dirname, 'tsconfig.lib.json')],
+    // }),
+    tsconfigPaths(),
+    libInjectCss(),
+    dts({ tsconfigPath: resolve(__dirname, 'tsconfig.lib.json') }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
@@ -16,9 +25,22 @@ export default defineConfig({
     },
     copyPublicDir: false,
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: [
+        'react',
+        'react-dom',
+        'tailwindcss',
+        '@carbon/icons-react',
+        '@radix-ui/react-dialog',
+        '@radix-ui/react-label',
+        '@radix-ui/react-popover',
+        '@radix-ui/react-scroll-area',
+        '@radix-ui/react-separator',
+        '@radix-ui/react-hover-card',
+        '@radix-ui/react-tooltip',
+        /^node:.*/,
+      ],
       input: Object.fromEntries(
-        glob.sync('lib/**/*.{ts,tsx}', { ignore: ['lib/**/*.d.ts', 'lib/components/**'] }).map((file) => [
+        glob.sync('lib/**/*.{ts,tsx}', { ignore: ['lib/**/*.d.ts'] }).map((file) => [
           //The name of the entry point
           // lib/nested/foo.ts becomes nested/foo
           relative('lib', file.slice(0, file.length - extname(file).length)),
