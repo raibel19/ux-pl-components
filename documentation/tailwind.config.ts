99a26789501c7a9dafc21a653c5bd0c4821c08b5
Polo <raibel_19@hotmail.com>
2025-06-10 07:02:07 -0600

    refactor: :fire: Update Tailwind configuration to enhance content paths and remove unused theme properties

--- documentation/tailwind.config.ts
+++ documentation/tailwind.config.ts
@@ -2,13 +2,16 @@ import type { Config } from 'tailwindcss';
 import tailwindcssAnimate from 'tailwindcss-animate';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
+import {presetConfig} from '../lib/tailwind.config-components';
+
 const config: Config = {
+  presets: [presetConfig],
   corePlugins: {
     preflight: false,
     container: false,
   },
   darkMode: ["class", '[data-theme="dark"]'],
-  content: ["./src/**/*.{jsx,tsx,html}", "../lib/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{jsx,tsx,html}", "../lib/**/*.{js,jsx,ts,tsx,md,mdx}", "./docs/**/*.{md,mdx}",],
   theme: {
      fontFamily: {
       sans: ['var(--font-roboto)', ...defaultTheme.fontFamily.sans],
@@ -22,103 +25,10 @@ const config: Config = {
     },
     extend: {
       colors: {
-        success: 'hsl(var(--success))',
-        border: 'hsl(var(--border))',
-        input: {
-          DEFAULT: 'hsl(var(--input))',
-          foreground: 'hsl(var(--input-foreground))',
-        },
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        dialog: {
-          header: 'hsl(var(--dialog-header))',
-        },
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        tertiary: {
-          DEFAULT: 'hsl(var(--tertiary))',
-          foreground: 'hsl(var(--tertiary-foreground))',
-        },
-        quaternary: {
-          DEFAULT: 'hsl(var(--quaternary))',
-          foreground: 'hsl(var(--quaternary-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-        table: {
-          foreground: 'hsl(var(--table-foreground))',
-          header: {
-            DEFAULT: 'hsl(var(--table-header))',
-            foreground: 'hsl(var(--table-header-foreground))',
-          },
-          body: {
-            DEFAULT: 'hsl(var(--table-body))',
-            'hover-bg': 'hsl(var(--table-body-hover-bg))',
-          },
-        },
-        sidebar: {
-          DEFAULT: 'hsl(var(--sidebar-background))',
-          foreground: 'hsl(var(--sidebar-foreground))',
-          primary: 'hsl(var(--sidebar-primary))',
-          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-          accent: 'hsl(var(--sidebar-accent))',
-          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-          border: 'hsl(var(--sidebar-border))',
-          ring: 'hsl(var(--sidebar-ring))',
-        },
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-      keyframes: {
-        'accordion-down': {
-          from: {
-            height: '0',
-          },
-          to: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-        },
-        'accordion-up': {
-          from: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-          to: {
-            height: '0',
-          },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
+        // border: 'hsl(var(--border))',
+        // foreground: 'hsl(var(--foreground))',
+        // background: 'hsl(var(--background))',
+      }
     },
   },
   plugins: [tailwindcssAnimate],
