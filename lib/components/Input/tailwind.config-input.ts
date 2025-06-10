5dfd5ad901b37571502e372d7ca0f3038b360a2e
Polo <raibel_19@hotmail.com>
2025-06-10 07:26:23 -0600

    feat(input): :sparkles: Add Input component styles and Tailwind CSS configuration

---
+++ lib/components/Input/tailwind.config-input.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss';
+
+const presetConfig: Partial<Config> = {
+  theme: {
+    extend: {
+      backgroundColor: (cnf) => ({
+        ...cnf.theme('colors'),
+        'ux-input-label': {
+          DEFAULT: 'hsl(var(--ux-input-label-background) / <alpha-value>)'
+        }
+      }),
+      textColor: (cnf) => ({
+        ...cnf.theme('colors'),
+        'ux-input-label': {
+          DEFAULT: 'hsl(var(--ux-input-label) / <alpha-value>)'
+        }
+      }),
+    }
+  }
+};
+
+export default presetConfig;
