fa5c6c233ebc3afe2eea09210369f15b558e47db
Polo <raibel_19@hotmail.com>
2025-06-23 06:56:28 -0600

    refactor: remove input components and related styles

--- lib/components/input-m/variants/input-addon-separator.variants.ts
+++
@@ -1,9 +0,0 @@
-import { cva } from 'class-variance-authority';
-
-export const separatorVariants = cva(['mx-2', 'h-[15px]'], {
-  variants: {
-    isError: {
-      true: ['bg-destructive'],
-    },
-  },
-});
