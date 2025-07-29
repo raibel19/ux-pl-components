a71238577000a2ec36ad2834e89fc4831ed425d4
Polo <raibel_19@hotmail.com>
2025-07-29 11:47:51 -0600

    feat(tailwind): ✨ Add `tailwindAutocomplete` to presets in Tailwind configuration
    * Included `tailwindAutocomplete` in the `presets` array of `presetConfig`.
    * This enhances the Tailwind configuration by integrating autocomplete styles.

--- lib/tailwind.config-components.ts
+++ lib/tailwind.config-components.ts
@@ -1,9 +1,10 @@
 import type { Config } from 'tailwindcss';
 
+import tailwindAutocomplete from './components/compositions/autocomplete/tailwind.config-autocomplete';
 import tailwindInput from './components/compositions/input/tailwind.config-input';
 
 export const presetConfig: Partial<Config> = {
-  presets: [tailwindInput],
+  presets: [tailwindInput, tailwindAutocomplete],
 };
 
 export const uxPlContentPaths: string[] = ['./node_modules/ux-pl-components/dist/**/*.{js,ts,jsx,tsx}'];
