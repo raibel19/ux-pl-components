5da8b1950ecf28e923e4b9ccae5fed038dcc1cfa
Polo <raibel_19@hotmail.com>
2025-07-30 10:10:56 -0600

    feat(autocomplete): ✨ Add `AutocompleteErrors` component for improved error handling
    * Introduced a new component `AutocompleteErrors` to display error messages in the autocomplete context.
    * Integrates with `useAutocompleteActionsContext` and `useAutocompleteContext` for dynamic error management.
    * Supports custom error messages through the `customMessageError` prop.

---
+++ lib/components/compositions/autocomplete/autocomplete-errors.tsx
@@ -0,0 +1,34 @@
+import { forwardRef } from 'react';
+
+import { cn } from '../../../lib/utils';
+import FieldMessage from '../../primitives/field-message';
+import { useAutocompleteActionsContext, useAutocompleteContext } from './context';
+
+interface AutocompleteErrorsProps extends React.HTMLAttributes<HTMLUListElement> {
+  className?: string;
+  customMessageError?: string;
+}
+
+export default forwardRef<HTMLUListElement, AutocompleteErrorsProps>(function AutocompleteErrors(props, ref) {
+  const { className, customMessageError, ...moreProps } = props;
+  const { isInvalid } = useAutocompleteContext();
+  const { errors } = useAutocompleteActionsContext();
+
+  const newErrors = errors;
+
+  if (customMessageError && isInvalid) {
+    newErrors.push(customMessageError);
+  }
+
+  if (!isInvalid || !newErrors.length) return null;
+
+  return (
+    <FieldMessage
+      ref={ref}
+      {...moreProps}
+      variant={{ type: 'error', size: 'sm' }}
+      messages={newErrors}
+      className={cn('mt-2 min-w-full max-w-min [text-wrap-style:pretty]', className || null)}
+    />
+  );
+});
